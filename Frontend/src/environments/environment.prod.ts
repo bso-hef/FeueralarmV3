b994dad6271763b003b265c7d5eea87c53224c9d
Marlon Landsiedel <marlon.landsiedel@p36.io>
2025-11-03 15:50:49 +0100

    Fixing apiURL und socketURL

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://52.59.245.175/api',
-  socketUrl: 'http://52.59.245.175',
+  apiUrl: 'https://alarm-bso.herokuapp.com/api',
+  socketUrl: 'https://alarm-bso.herokuapp.com',
   appVersion: '2.0.0',
   appName: 'Feueralarm BSO',
   enableTestLogin: false,
