4fe504fb45accb7ecdf75473791976ff19614fd7
Marlon Landsiedel <marlon.landsiedel@p36.io>
2025-11-04 12:51:07 +0100

    Enable test login and URLs changed

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
   // use the deployed backend IP and port (adjust port if your backend listens on a different port)
-  apiUrl: 'http://52.59.245.175:3000/api',
-  socketUrl: 'http://52.59.245.175:3000',
+  apiUrl: 'http://localhost:3000/api',
+  socketUrl: 'http://localhost:3000',
   appVersion: '2.0.0',
   appName: 'Feueralarm BSO',
-  enableTestLogin: false,
+  enableTestLogin: true,
 };
