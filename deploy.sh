a00ca9676c0e959cd6517147cf18e481e7eb7661
Marlon Landsiedel <marlon.landsiedel@p36.io>
2025-11-16 11:15:49 +0100

    Updating deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ APP_ROOT="/var/www/Deployment"
 REPO_DIR="$APP_ROOT/Repository"
 FRONTEND_DIR="$REPO_DIR/Frontend"
 WEBROOT="$FRONTEND_DIR/www"                   # Webserver zeigt DIREKT hierhin
-BRANCH="UAP-4.2.2-Fotos-&-Notizen-anhängen"
+BRANCH="UAP6.1-Exportfunktionen"
 REPO_SSH="git@github.com:MarlonH05/FeueralarmV3.git"
 
 echo "==> Check SSH-Verbindung zu $REMOTE"
