70cddb6f2ff9c453a390337c28e6711018aeff15
Marlon Landsiedel <marlon.landsiedel@p36.io>
2025-11-16 19:56:31 +0100

    setting environment from http --> https

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://18.193.97.54/api',
-  socketUrl: 'http://18.193.97.54',
+  apiUrl: 'https://18.193.97.54/api',
+  socketUrl: 'https://18.193.97.54',
   appVersion: '3.0.0',
   appName: 'Feueralarm BSO',
   enableTestLogin: true,
